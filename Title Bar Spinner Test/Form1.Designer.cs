974f7c6280496a2902c8429f07377c7782d1710f
mastercodeon314 <78676320+mastercodeon314@users.noreply.github.com>
2021-06-15 15:51:25 -0500

    main upload

---
+++ Title Bar Spinner Test/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Title_Bar_Spinner_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spinnerCtrlBtn = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // spinnerCtrlBtn
+            // 
+            this.spinnerCtrlBtn.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (32)))), ((int) (((byte) (32)))), ((int) (((byte) (32)))));
+            this.spinnerCtrlBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.spinnerCtrlBtn.Location = new System.Drawing.Point(15, 90);
+            this.spinnerCtrlBtn.Name = "spinnerCtrlBtn";
+            this.spinnerCtrlBtn.Size = new System.Drawing.Size(97, 23);
+            this.spinnerCtrlBtn.TabIndex = 0;
+            this.spinnerCtrlBtn.Text = "Start Spinner";
+            this.spinnerCtrlBtn.UseVisualStyleBackColor = false;
+            this.spinnerCtrlBtn.Click += new System.EventHandler(this.spinnerCtrlBtn_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (32)))), ((int) (((byte) (32)))), ((int) (((byte) (32)))));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {"Dots", "Dots2", "Dots3", "Dots4", "Dots5", "Dots8Bit", "Dots9", "Dots10", "Dots11", "Dots12", "Progress7", "Progress10", "Line"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Spinner Selection";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (32)))), ((int) (((byte) (32)))), ((int) (((byte) (32)))));
+            this.ClientSize = new System.Drawing.Size(331, 125);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.spinnerCtrlBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button spinnerCtrlBtn;
+
+        #endregion
+    }
+}
---
+++ Title Bar Spinner Test/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleCore.Cli;
+namespace Title_Bar_Spinner_Test
+{
+    public partial class Form1 : Form
+    {
+        private FormSpinner spinner;
+        public Form1()
+        {
+            InitializeComponent();
+
+            spinner = new FormSpinner(this);
+            spinner.Current = FormSpinner.Dots;
+            this.comboBox1.SelectedIndex = 0;
+        }
+
+        private void spinnerCtrlBtn_Click(object sender, EventArgs e)
+        {
+            if (spinner.IsRunning == false)
+            {
+                spinner.Start();
+                spinnerCtrlBtn.Text = "Stop Spinner";
+            }
+            else
+            {
+                spinner.Stop();
+                spinnerCtrlBtn.Text = "Start Spinner";
+            }
+        }
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = this.comboBox1.SelectedItem as string;
+            switch (selectedItem)
+            {
+                
+                case "Dots":
+                {
+                    spinner.Current = FormSpinner.Dots;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots2":
+                {
+                    spinner.Current = FormSpinner.Dots2;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots3":
+                {
+                    spinner.Current = FormSpinner.Dots3;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots4":
+                {
+                    spinner.Current = FormSpinner.Dots4;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots5":
+                {
+                    spinner.Current = FormSpinner.Dots5;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots8Bit":
+                {
+                    spinner.Current = FormSpinner.Dots8Bit;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots9":
+                {
+                    spinner.Current = FormSpinner.Dots9;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots10":
+                {
+                    spinner.Current = FormSpinner.Dots10;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots11":
+                {
+                    spinner.Current = FormSpinner.Dots11;
+                    spinner.Reset();
+                    break;
+                }
+                case "Dots12":
+                {
+                    spinner.Current = FormSpinner.Dots12;
+                    spinner.Reset();
+                    break;
+                }
+                case "Progress7":
+                {
+                    spinner.Current = FormSpinner.Progress7;
+                    spinner.Reset();
+                    break;
+                }
+                case "Progress10":
+                {
+                    spinner.Current = FormSpinner.Progress10;
+                    spinner.Reset();
+                    break;
+                }
+                case "Line":
+                {
+                    spinner.Current = FormSpinner.Line;
+                    spinner.Reset();
+                    break;
+                }
+            }
+        }
+    }
+}
